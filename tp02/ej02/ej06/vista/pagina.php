fb5b984158fa658245fef928e04af7c97286d914
pgualda <111656559+pgualda@users.noreply.github.com>
2022-08-21 17:30:39 -0300

    Add files via upload

---
+++ tp02/ej02/ej06/vista/pagina.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datos personales</title>
+    <link rel="stylesheet" href="../../../bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <script src="../../../bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
+<body>
+    
+</body>
+</html>
+
+<h3>Datos personales</h3>
+
+<form id="presentacion" name="presentacion" method="get" action="../control/pagina.php" class="row g-3 needs-validation" novalidate>
+
+<div class="col-md-4">
+            <label for="nombre" class="form-label">Nombre</label>
+            <input type="text" class="form-control" id="nombre" value="" name="nombre" required>
+    <div class="valid-feedback">
+        Correcto!
+    </div>
+</div>
+
+<!--Nombre: <input name="nombre" id="nombre" type="text" /> -->
+
+<div class="col-md-4">
+            <label for="apellido" class="form-label">Apellido</label>
+            <input type="text" class="form-control" id="apellido" value="" name="apellido" required>
+    <div class="valid-feedback">
+        Correcto!
+    </div>
+</div>
+
+<!--Apellido: <input name="apellido" id="apellido" type="text" /> -->
+
+<div class="col-md-4">
+            <label for="edad" class="form-label">Edad</label>
+            <input type="number" class="form-control" id="edad" value="" name="edad" required>
+    <div class="valid-feedback">
+        Correcto!
+    </div>
+</div>
+
+<!--Edad: <input name="edad" id="edad" type="number" /> -->
+
+<div class="col-md-4">
+            <label for="direccion" class="form-label">Direccion</label>
+            <input type="text" class="form-control" id="direccion" value="" name="direccion" required>
+    <div class="valid-feedback">
+        Correcto!
+    </div>
+</div>
+
+<!--Direccion: <input name="direccion" id="direccion" type="text" /> -->
+<fieldset> 
+    <legend>Selecciona tu nivel de estudios</legend>
+<div class="form-check mb-3">
+    <input type="radio" class="form-check-input" id="sinestudio" name="nivelest" value="sin estudios" required>
+    <label class="form-check-label" for="sinestudio">Sin estudios</label>
+</div>
+<div class="form-check mb-3">
+    <input type="radio" class="form-check-input" id="solopri" name="nivelest" value="solo primaria">
+    <label class="form-check-label" for="solopri">Solo primaria</label>
+</div>
+<div class="form-check mb-3">
+    <input type="radio" class="form-check-input" id="secundario" name="nivelest" value="secundario">
+    <label class="form-check-label" for="secundario">Secundario</label>
+    <div class="invalid-feedback">debe ingressar una opcion</div>
+</div>
+</fieldset> 
+
+<!-- <input type="radio" id="sinestudio" name="nivelest" value="sin estudios">
+        <label class="radio" for="sinestudio">Sin estudios</label><br>
+        <input type="radio" id="solopri" name="nivelest" value="primaria">
+        <label class="radio" for="solopri">Solo primaria</label><br>
+        <input type="radio" id="secundario" name="nivelest" value="secundario">
+        <label class="radio" for="secundario">Secundario</label><br> -->
+<fieldset> 
+    <legend>Selecciona tu genero</legend>
+<div class="form-check mb-3">
+    <input type="radio" class="form-check-input" id="masculino" name="genero" value="masculino" required>
+    <label class="form-check-label" for="masculino">Masculino</label>
+</div>
+<div class="form-check mb-3">
+    <input type="radio" class="form-check-input" id="femenino" name="genero" value="femenino">
+    <label class="form-check-label" for="femenino">Femenino</label>
+    <div class="invalid-feedback">debe ingressar una opcion</div>
+</div>
+</fieldset> 
+
+<!--    <input type="radio" id="femenino" name="genero" value="femenino">
+        <label class="radio" for="femenino">Femenino</label><br>
+        <input type="radio" id="masculino" name="genero" value="masculino">
+        <label class="radio" for="masculino">Masculino</label><br> -->
+
+<fieldset> 
+    <legend>Selecciona los deportes que practicas</legend>
+<div class="form-check mb-3">
+    <input type="checkbox" class="form-check-input" id="futbol" name="deporte[]" value="futbol">
+    <label class="form-check-label" for="futbol">Futbol</label>
+</div>
+<div class="form-check mb-3">
+    <input type="checkbox" class="form-check-input" id="voley" name="deporte[]" value="voley">
+    <label class="form-check-label" for="voley">Voley</label>
+</div>
+<div class="form-check mb-3">
+    <input type="checkbox" class="form-check-input" id="basquet" name="deporte[]" value="basquet">
+    <label class="form-check-label" for="basquet">Basquet</label>
+    <div class="invalid-feedback">datos invalidos</div>
+</div>
+</fieldset> 
+
+
+<!--        <fieldset> 
+        <legend>Selecciona los deportes que practicas</legend>
+           <div>
+               <input type="checkbox" id="futbol" name="deporte[]" value="futbol">
+               <label for="futbol">Futbol</label>
+            </div>
+            <div>
+               <input type="checkbox" id="voley" name="deporte[]" value="voley">
+               <label for="voley">Voley</label>
+            </div>
+            <div>
+               <input type="checkbox" id="basquet" name="deporte[]" value="basquet">
+               <label for="basquet">Futbol</label>
+            </div>
+        </fieldset> -->
+
+
+<div class="col-12">
+     <button class="btn btn-primary" type="submit">Enviar</button>
+</div>
+
+<!--    <input type="submit" name="submit" value="Enviar"/> -->
+
+</form>
+
+</body>    
+
+<script>
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+</script>
+
+</html>
+
+
+<!--
+<form id="vernro" name="vernro" method="post" action="../control/vernumero.php" class="row g-3 needs-validation" novalidate>
+        <div class="col-md-4">
+            <label for="nro1" class="form-label">Numero</label>
+            <input type="number" class="form-control" id="nro1" value="0" required>
+            <div class="valid-feedback">
+              Correcto!
+            </div>
+          </div>
+
+
+    <div class="col-12">
+         <button class="btn btn-primary" type="submit">Enviar</button>
+    </div>
+
+ <input type="radio" id="sinestudio" name="nivelest" value="sin estudios">
+        <label class="radio" for="sinestudio">Sin estudios</label><br>
+        <input type="radio" id="solopri" name="nivelest" value="primaria">
+        <label class="radio" for="solopri">Solo primaria</label><br>
+        <input type="radio" id="secundario" name="nivelest" value="secundario">
+        <label class="radio" for="secundario">Secundario</label><br>
+
+        <input type="radio" id="femenino" name="genero" value="femenino">
+        <label class="radio" for="femenino">Femenino</label><br>
+        <input type="radio" id="masculino" name="genero" value="masculino">
+        <label class="radio" for="masculino">Masculino</label><br>
+
+        <fieldset> 
+        <legend>Selecciona los deportes que practicas</legend>
+           <div>
+               <input type="checkbox" id="futbol" name="deporte[]" value="futbol">
+               <label for="futbol">Futbol</label>
+            </div>
+            <div>
+               <input type="checkbox" id="voley" name="deporte[]" value="voley">
+               <label for="voley">Voley</label>
+            </div>
+            <div>
+               <input type="checkbox" id="basquet" name="deporte[]" value="basquet">
+               <label for="basquet">Futbol</label>
+            </div>
+        </fieldset> 
+
+
+-->
+
---
+++ tp02/ej02/ej07/vista/pagina.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Calculadora</title>
+    <link rel="stylesheet" href="../../../bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <script src="../../../bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+    
+</body>
+</html>
+<form id="calculadora" name="calculadora" method="get" action="../control/pagina.php" class="row g-3 needs-validation" novalidate>
+
+<div class="col-md-4">
+    <label for="op1" class="form-label">Operador 1</label>
+    <input type="number" class="form-control" id="op1" value=0 name="op1" required>
+    <div class="valid-feedback">
+        Correcto!
+    </div>
+</div>
+
+<!-- <input name="op1" id="op1" type="number" /><br/> -->
+
+<div class="col-md-4">
+    <label for="op2" class="form-label">Operador 2</label>
+    <input type="number" class="form-control" id="op2" value=0 name="op2" required>
+    <div class="valid-feedback">
+        Correcto!
+    </div>
+</div>
+
+<!-- <input name="op2" id="op2" type="number" /><br/> -->
+
+<div class="mb-3">
+    <select class="form-select" required aria-label="Seleccione operador">
+      <option value="suma">Suma</option>
+      <option value="resta">Resta</option>
+      <option value="multiplicacion">Multiplicacion</option>
+    </select>
+    <div class="invalid-feedback">Debe seleccionar una opcion!</div>
+  </div>
+
+<!-- <select name='operacion' id='operacion' style='width: 408px'>;
+        <option value="suma">Suma</option>";
+        <option value="resta">Resta</option>";
+        <option value="multiplicacion">Multiplicacion</option>"; -->
+
+
+<div class="col-12">
+     <button class="btn btn-primary" type="submit">Enviar</button>
+</div>
+
+<!--    <input type="submit" name="submit" value="Enviar"/> -->
+
+</form>
+
+</body>    
+
+<script>
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+</script>
+
+</html>
