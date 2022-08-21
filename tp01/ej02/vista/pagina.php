47b5efdf34036b74fadbe2550f2c19d96cfff52d
pgualda <111656559+pgualda@users.noreply.github.com>
2022-08-21 13:05:04 -0300

    Add files via upload

---
+++ tp01/ej02/vista/pagina.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Horas diarias de web dinamica</title>
+</head>
+<body>
+
+<form id="webdinamica" name="webdinamica" method="get" action="../control/pagina.php">
+        Lunes: <input name="dia1" id="dia1" type="number" /><br/>
+        Martes: <input name="dia2" id="dia2" type="number" /><br/>
+        Miercoles: <input name="dia3" id="dia3" type="number" /><br/>
+        Jueves: <input name="dia4" id="dia4" type="number" /><br/>
+        Viernes: <input name="dia5" id="dia5" type="number" /><br/>
+        <input type="submit" name="submit" value="Aceptar"/>
+</form>
+
+</body>    
+
+</html>
---
+++ tp01/ej05/vista/pagina.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datos personales</title>
+    <link rel="stylesheet" href="css/style.css">
+<body>
+    
+</body>
+</html>
+<form id="presentacion" name="presentacion" method="get" action="../control/pagina.php">
+        Nombre: <input name="nombre" id="nombre" type="text" /><br/><br/>
+        Apellido: <input name="apellido" id="apellido" type="text" /><br/><br/>
+        Edad: <input name="edad" id="edad" type="number" /><br/><br/>
+        Direccion: <input name="direccion" id="direccion" type="text" /><br/><br/>
+ <!-- nivel de estudios -->
+ <input type="radio" id="sinestudio" name="nivelest" value="sin estudios">
+        <label class="radio" for="sinestudio">Sin estudios</label><br>
+        <input type="radio" id="solopri" name="nivelest" value="primaria">
+        <label class="radio" for="solopri">Solo primaria</label><br>
+        <input type="radio" id="secundario" name="nivelest" value="secundario">
+        <label class="radio" for="secundario">Secundario</label><br>
+        <br/>
+        <!-- shender -->
+        <input type="radio" id="femenino" name="genero" value="femenino">
+        <label class="radio" for="femenino">Femenino</label><br>
+        <input type="radio" id="masculino" name="genero" value="masculino">
+        <label class="radio" for="masculino">Masculino</label><br>
+        <br/>
+        <input type="submit" name="submit" value="Enviar"/>
+
+</form>
+
+</body>    
+
+</html>
+
+
