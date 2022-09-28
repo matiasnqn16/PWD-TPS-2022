a82961461dbf4343d16933d577b12b44b810d33d
FMatias <matiasfarfan61@gmail.com>
2022-09-28 14:00:21 -0300

    Update configuracion.php

--- TP4/configuracion.php
+++ TP4/configuracion.php
@@ -14,10 +14,10 @@ include_once($ROOT.'util/funciones.php');
 
 
 // Variable que define la pagina de autenticacion del proyecto
-$INICIO = "Location:http://".$_SERVER['HTTP_HOST']."/$PROYECTO/vista/login/login.php";
+$INICIO = "Location:http://".$_SERVER['HTTP_HOST']."/$PROYECTO/TP4/Vista/index.php";
 
 // variable que define la pagina principal del proyecto (menu principal)
-$PRINCIPAL = "Location:http://".$_SERVER['HTTP_HOST']."/$PROYECTO/principal.php";
+$PRINCIPAL = "Location:http://".$_SERVER['HTTP_HOST']."/$PROYECTO/TP4/Vista/index.php";
 
 
 $_SESSION['ROOT']=$ROOT;
