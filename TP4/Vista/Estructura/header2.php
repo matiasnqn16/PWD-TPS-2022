334d03c0b104aaa6436f9a24f9ba148139f65c26
FMatias <matiasfarfan61@gmail.com>
2022-09-28 14:02:55 -0300

    Cambio de ubicacion
    de paso de la carpeta util a vista

---
+++ TP4/Vista/Estructura/header2.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Menu</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.0-beta1 -->
+
+  <link rel="stylesheet" href="../../util/bootstrap-5.2.0-dist/css/bootstrap.min.css">
+<!--   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"> -->
+<!-- css simple -->
+<link rel="stylesheet" href="../css/estilos.css">
+</head>
+
+<body style="background-color: #efefef;">
+  <header>
+    <!-- place navbar here -->
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+          <div class="container">
+            <a class="navbar-brand" href="../../../TP4/Vista/index.php">InfoAutos</a>
+            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+                <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../vista/VerAutos.php">Ver Autos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../vista/buscarAuto.php">Buscar Autos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../vista/listaPersonas.php">Listar Personas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../vista/NuevaPersona.php">Agregar Persona</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../vista/NuevoAuto.php">Agregar Auto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../vista/CambioDuenio.php">Cambiar Dueño</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../Vista/buscarPersona.php">Buscar Personas</a>
+                    </li>
+                    
+                    
+                </ul>
+                
+            </div>
+      </div>
+    </nav>
+    
+  </header>
+
+  <script src="../util/bootstrap-5.2.0-dist/js/bootstrap.min.js"></script>
+  <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js"
+    integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous">
+  </script> -->
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"
+    integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
